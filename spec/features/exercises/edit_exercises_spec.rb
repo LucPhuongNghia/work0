fdc39742ce2a8cdd3d40f064647b043f97dd79f0
nghia <nghialp@tocdep.vn>
2016-12-07 09:39:29 +0000

    Write edit exercise spec

---
+++ spec/features/exercises/edit_exercises_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.feature "Edit exercise" do
+    before do
+       @owner = User.create!(first_name: "John", last_name: "Doe", email: "owner@example.com", password: "password")
+    
+       @owner_exercise = @owner.exercises.create!(duration_in_min: 48,
+                                                workout: "My body building activity",
+                                                workout_date: Date.today)
+                                                
+       login_as(@owner) 
+    end
+    
+    scenario "with valid data succeeds" do
+       visit "/"
+       click_link "My lounge"
+       
+       path = "/users/#{@owner.id}/exercises/#{@owner_exercise.id}/edit"
+       link = "a[href=\'#{path}'\]"
+       find(link).click
+       
+       fill_in "Duration", with: 45
+       click_button "Update exercise"
+       
+       expect(page).to have_content("Exercise has been updated")
+       expect(page).to have_content(45)
+       expect(page).not_to have_content(48)
+    end
+    
+end

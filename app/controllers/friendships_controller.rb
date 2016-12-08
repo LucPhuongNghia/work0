accb7070ead51f40f27145facd2a603a59f8758d
nghia <nghialp@tocdep.vn>
2016-12-08 09:56:47 +0000

    Set unfollow friend feature

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -13,6 +13,17 @@ class FriendshipsController < ApplicationController
       Friendship.create(friendship_params) unless current_user.follow_or_same?(friend)
    end
    
+   def destroy
+      @friendship = Friendship.find(params[:id])
+      friend = @friendship.friend.full_name
+      if @friendship.destroy
+         flash[:notice] = "#{friend} unfollowed"
+      else
+         flash.now[:alert] = "#{friend} could not be unfollowed"
+      end
+      redirect_to user_exercises_path(current_user)
+   end
+   
    private
    def friendship_params
       params.permit(:user_id, :friend_id) 
